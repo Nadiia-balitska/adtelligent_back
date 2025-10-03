fd586dc8df6755dc4adc76bc6b9f7ec748888782
Nadiia Balitska <161743763+Nadiia-balitska@users.noreply.github.com>
2025-10-03 18:01:30 +0200

    deleted prefix api for route

--- src/routes/feed.route.ts
+++ src/routes/feed.route.ts
@@ -3,7 +3,7 @@ import { getFeedDataRoutes } from "../modules/feedParser/routes/feedParser.route
 import fetchFeedRoutes from "../modules/feedFetcher/routes/fetchFeed.route";
 
 const routes: FastifyPluginAsync = async (fastify) => {
-  await fastify.register(getFeedDataRoutes, { prefix: "/api" });
+  await fastify.register(getFeedDataRoutes);
 
 
   await fastify.register(fetchFeedRoutes, { prefix: "/api" });
